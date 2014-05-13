f257b2078ec4e5da5fc2ea744c60b18c376a3499
Matheus Cunha <mathcunha@gmail.com>
2014-05-13 00:39:29 +0000

    statistics by request type

--- examples/wordpress/generate_report.rb
+++ examples/wordpress/generate_report.rb
@@ -4,40 +4,71 @@ require 'logger'
 
 class GenerateReport
 
-  def initialize(server_ip:'54.82.195.19', server_port:9200)
+  def initialize(server_ip:'127.0.0.1', server_port:9200)
      @server_ip = server_ip
      @server_port = server_port
      @workloads = [100,200,300,400,500,600,700,800,900,1000]
      @profile_ids = ['c3_large', 'c3_xlarge', 'c3_2xlarge']
+     @requests = ['new post', 'view post', 'edit post', 'find posts']
      @logger = Logger.new('logfile.log')
   end
 
   def run
-    puts "provider_id,#,workload,min,max,mean,count"
+    puts "provider_id,instance,workload,request,min,max,mean,median,count"
     for j in 1..4
       @profile_ids.each do |profile_id|
         @workloads.each do |workload|
-           next if j == 3
-           obj = get_statistics("#{j}_#{profile_id}",workload)
-           puts "#{profile_id},#{j},#{workload},#{obj["facets"]["stat1"]["min"]},#{obj["facets"]["stat1"]["max"]},#{obj["facets"]["stat1"]["mean"]},#{obj["facets"]["stat1"]["count"]}"
+           @requests.each do |request|
+             obj = get_statistics("#{j}_#{profile_id}",workload,request)
+             median = get_median("#{j}_#{profile_id}", workload, request, obj["hits"]["total"])
+             puts "#{profile_id},#{j},#{workload},#{request},#{obj["facets"]["stat1"]["min"]},#{obj["facets"]["stat1"]["max"]},#{obj["facets"]["stat1"]["mean"]},#{median},#{obj["facets"]["stat1"]["count"]}"
+          end
         end
       end
     end
   end
 
-  def get_statistics(scenario, workload)
+  def get_statistics(scenario, workload, request)
     uri = URI("http://#{@server_ip}:#{@server_port}/_search?pretty=1")
     req = Net::HTTP::Post.new(uri)#, initheader = {'Content-Type' =>'text/plain'})
     #req.body = get_body_rt_gt_0(scenario, workload)
-    req.body = get_body(scenario, workload)
+    req.body = get_body(scenario, workload, request)
     res = Net::HTTP.start(uri.hostname, uri.port) do |http|
       http.request(req)
     end
     JSON.parse(res.body)
   end
-  def get_body(scenario, workload)
+
+  def get_median(scenario, workload, request, hits)
+    median_pos = hits / 2
+    uri = URI("http://#{@server_ip}:#{@server_port}/_search?pretty=1")
+    req = Net::HTTP::Post.new(uri)
+
+    #puts "#{hits} - #{median_pos}"
+
+    req.body = get_body(scenario, workload, request, from:median_pos)
+
+    res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(req)
+    end
+
+    obj = JSON.parse(res.body)
+
+    if hits % 2 == 1
+      obj["hits"]["hits"][1]["_source"]["response_time"]
+    else
+       (obj["hits"]["hits"][0]["_source"]["response_time"] + obj["hits"]["hits"][1]["_source"]["response_time"]) / 2.0
+    end
+    
+  end
+
+  def get_body(scenario, workload, request, from:0)
      body = ""
      	body += "{\n"
+
+        body += "   \"sort\" : [\n"
+        body += "      {\"response_time\" : {\"order\" : \"asc\"}}\n"
+        body += "   ],\n"
 	body += "   \"query\" : {\n"
 	body += "      \"bool\" : {\n"
 	body += "         \"must\" : [            \n"
@@ -55,18 +86,22 @@ class GenerateReport
 	body += "               \"term\" : {\n"
 	body += "                  \"workload\" : \"#{workload}\"\n"
 	body += "               }\n"
-	body += "            }\n"
-#        body += "	     ,{\n"
-#	body += "		 \"query_string\" : {\n"
-#        body += "                 \"fields\" : [\"status\"],\n"
-#        body += "                 \"query\" : \"OK\"\n"
-#        body += "                }\n"
-#	body += "	     }\n"
+	body += "            },\n"
+        body += "			{\n"
+        body += "			   \"match_phrase\" : {\n"
+        body += "				  \"status\" : \"OK\"\n"
+        body += "			   }\n"
+        body += "			},\n"
+        body += "			{\n"
+        body += "			   \"match_phrase\" : {\n"
+        body += "				  \"request\" : \"#{request}\"\n"
+        body += "			   }\n"
+        body += "			}\n"
 	body += "         ]\n"
 	body += "      }\n"
 	body += "   },\n"
-	body += "   \"from\" : 0,\n"
-	body += "   \"size\" : 0,\n"
+	body += "   \"from\" : #{from},\n"
+	body += "   \"size\" : 2,\n"
 	body += "   \"facets\" : {\n"
 	body += "        \"stat1\" : {\n"
 	body += "            \"statistical\" : {\n"
@@ -75,6 +110,9 @@ class GenerateReport
 	body += "        }\n"
 	body += "    }\n"
 	body += "}"
+        #puts "#{body}"
+
+        body
   end
 
   def get_body_rt_gt_0(scenario, workload)
