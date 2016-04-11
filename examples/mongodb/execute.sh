dfe07bc81b0c6b99021c413157ed7bd5a59bc7d6
Matheus Cunha <mathcunha@gmail.com>
2016-04-11 23:29:19 +0000

    change results dir

--- examples/mongodb/execute.sh
+++ examples/mongodb/execute.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 folder=$(date +%s%3N)
-ins="insert_$1_$2_$folder.log"
-wkl="wkl_$1_$2_$folder.log"
+ins="/data/insert_$1_$2_$folder.log"
+wkl="/data/wkl_$1_$2_$folder.log"
 ~/ycsb-0.7.0/bin/ycsb load mongodb -s -P ~/ycsb-0.7.0/workloads/workloada -p recordcount=$2 -threads 4 >> $ins
 ~/ycsb-0.7.0/bin/ycsb run mongodb -s -P ~/ycsb-0.7.0/workloads/workloada -p operationcount=$2 -threads 4 >> $wkl
 exit 0
