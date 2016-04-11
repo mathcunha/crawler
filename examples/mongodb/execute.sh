589b73537dd739499ca8b5f9ac7b3211a1195ad0
Matheus Cunha <mathcunha@gmail.com>
2016-04-11 21:38:50 +0000

    mongodb first test

--- examples/mongodb/execute.sh
+++ examples/mongodb/execute.sh
@@ -1,2 +1,7 @@
-./bin/ycsb load mongodb -s -P workloads/workloada -p recordcount=10000000 -threads 4 >> insert.log
-./bin/ycsb run mongodb -s -P workloads/workloada -p operationcount=10000000 -threads 4 >> workload.log
+#!/bin/bash
+folder=$(date +%s%3N)
+ins="insert_$1_$2_$folder.log"
+wkl="wkl_$1_$2_$folder.log"
+~/ycsb-0.7.0/bin/ycsb load mongodb -s -P ~/ycsb-0.7.0/workloads/workloada -p recordcount=$2 -threads 4 >> $ins
+~/ycsb-0.7.0/bin/ycsb run mongodb -s -P ~/ycsb-0.7.0/workloads/workloada -p operationcount=$2 -threads 4 >> $wkl
+exit 0
